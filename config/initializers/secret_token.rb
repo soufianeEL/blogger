cabeaba57a56167dc4bc274be8eab626df4a4c5f
soufiane <soufianeelhamchi@gmail.com>
2014-03-19 12:22:48 +0000

    creating the app + g artilces model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = '3ec3d67e38e94240cab0ead8bde4723b677779a86daade45c02517eece8fcf47f354d96a9533a19bd6679d4b74d01646acf7bbebbe26a144f015cd0a2172b65b'
